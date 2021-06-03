a3cb6587f7909cab24e7284dd14c8ea4fe2541cc
sophiagavrila <msophiagavrila@gmail.com>
2021-06-03 00:12:52 -0400

    initial push

---
+++ src/main/java/com/fdflib/example/model/Car.java
@@ -0,0 +1,26 @@
+package com.fdflib.example.model;
+
+import com.fdflib.annotation.FdfIgnore;
+import com.fdflib.model.state.CommonState;
+
+public class Car extends CommonState {
+
+    public CarMake make = null;
+    public String model = "";
+    public Integer year = 0;
+    public String color = "";
+    public String name = "";
+    public String description = "";
+    public Boolean isInNeedOfRepair = true;
+    public Boolean isOnCall = true;
+    public Boolean isOutWorking = false;
+    public long currentDriverId = -1L;
+
+    @FdfIgnore
+    public Driver currentDriver = null;
+
+    public Car() {
+        super();
+    }
+
+}
---
+++ src/main/java/com/fdflib/example/model/Driver.java
@@ -0,0 +1,14 @@
+package com.fdflib.example.model;
+
+import com.fdflib.model.state.CommonState;
+
+public class Driver extends CommonState {
+	
+    public String firstName = "";
+    public String lastName = "";
+    public String phoneNumber = "";
+
+    public Driver() {
+        super();
+    }
+}
